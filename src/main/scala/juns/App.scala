d25a6dc504d6f13313e1f4595ee4679337ab6357
INSAM <zoony82@gmail.com>
2018-09-30 09:16:44 +0900

    17년 1년치 부동산 실거래가 추출 및 file로 저장

--- src/main/scala/juns/App.scala
+++ src/main/scala/juns/App.scala
@@ -1,7 +1,11 @@
 package juns
 
-import scala.io.Source
 import scala.collection.mutable.Map
+import scala.io.Source
+import java.io.File
+import java.io.PrintWriter
+
+import scala.xml.{Elem, Node}
 
 /**
  * @author ${user.name}
@@ -11,33 +15,36 @@ object App {
   def foo(x : Array[String]) = x.foldLeft("")((a,b) => a + b)
   
   def main(args : Array[String]) {
-    println( "Hello World!" )
-    println("concat arguments = " + foo(args))
+    println( "land anslysis start!" )
 
     // road service key
-    val serviceKey = Source.fromFile("C:\\Users\\INSAM\\IdeaProjects\\land\\src\\main\\scala\\juns\\service_key.txt").mkString
+    val serviceKey: String = Source.fromFile("C:\\Users\\INSAM\\IdeaProjects\\land\\src\\main\\scala\\juns\\service_key.txt").mkString
 
     // road raw data from api
-    val api = new ApiExplorer()
-    val real =api.getRealEstate("11110","201805", serviceKey)
-//    println(real)
-    //<거래금액>    66,500</거래금액><건축년도>2007</건축년도><년>2018</년><법정동> 필운동</법정동><아파트>두레엘리시안</아파트><월>5</월><일>11~20</일><전용면적>84.74</전용면적><지번>174-1</지번><지역코드>11110</지역코드><층>6</층></item><item>
-    val xmlRaw = scala.xml.XML.loadString(real)
-    println(xmlRaw)
+    val api: ApiExplorer = new ApiExplorer()
 
+    val realSet: Array[Int] = (201701 to 201712).toArray
     var resMap: Map[String, String] = Map()
 
-    for{
-      item <- xmlRaw \\ "item"
-      aptName <- item \ "아파트"
-      money <- item \ "거래금액"
-    } yield {
-//      println("아파트 명칭" + aptName + "거래 금액" + money)
-      resMap += (aptName.toString() -> money.toString())
+    realSet.foreach { v =>
+      val real: String =api.getRealEstate("11110", v.toString(), serviceKey)
+      //<거래금액>    66,500</거래금액><건축년도>2007</건축년도><년>2018</년><법정동> 필운동</법정동><아파트>두레엘리시안</아파트><월>5</월><일>11~20</일><전용면적>84.74</전용면적><지번>174-1</지번><지역코드>11110</지역코드><층>6</층></item><item>
+      val xmlRaw: Elem = scala.xml.XML.loadString(real)
+
+      for{
+        item: Node <- xmlRaw \\ "item"
+        aptName: Node <- item \ "아파트"
+        money: Node <- item \ "거래금액"
+      } yield {
+        //      println("아파트 명칭" + aptName + "거래 금액" + money)
+        resMap += (v.toString() + aptName.toString() -> money.toString())
+      }
     }
 
-    resMap.foreach(v => println(v))
+    val writer: PrintWriter = new PrintWriter(new File("aptMoney.txt"))
 
+    resMap.foreach(v => writer.write(v._1+":"+v._2 + "\n"))
+    writer.close()
 
     // Data preprocessing with spark core lib
     // analysis data
